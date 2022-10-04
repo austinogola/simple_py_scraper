1fbdbe191f44096a6a46a82b71149cd31a9e5bfb
Austin <austinandogola@gmail.com>
2022-10-04 17:42:36 +0300

    configured selenium

--- scraper.py
+++ scraper.py
@@ -8,7 +8,12 @@ from bs4 import BeautifulSoup
 
 def web_scrape(url,xpath):
     options = Options()
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument("start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
     driver=webdriver.Chrome(chrome_options=options,executable_path=ChromeDriverManager().install())
